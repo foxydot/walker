446b238a7b39b6cd2cc092e20bb2138715e5fb0a
Catherine OBrien-Sandrick <catherine@madsciencedept.com>
2013-10-02 11:56:36 -0400

    remove dozens of unneccessary plugins

--- wp-content/plugins/msd-cpt-framework/template/success.php
+++
@@ -1,4 +0,0 @@
-<div class="my_meta_control">
- 		<?php $mb->the_field('subtitle'); ?>
-		<p><input type="text" name="<?php $mb->the_name(); ?>" value="<?php $mb->the_value(); ?>"/></p>
- </div>
--- wp-content/plugins/msd-txtwdgt-click/js/msd-widget-text.js
+++
@@ -1,9 +0,0 @@
-jQuery(document).ready(function($) {	
-	$('.msd-widget-text').parent().css('position','relative');
-	$('.msd-widget-text').height(function(){
-		return $(this).parent().height();
-	});
-	$('.msd-widget-text').width(function(){
-		return $(this).parent().width();
-	});
-});
