b2ee8a03cb921f8f9b113dd01c56d783022fa31e
Catherine OBrien-Sandrick <catherine@madsciencedept.com>
2014-04-14 12:20:13 -0400

    Still having trouble with disappearing affiliate logos in the footer widgets when I refresh the page Is there a specific browser where you are seeing this? I can’t recreate. I am seeing it on Chrome v34 and Safari 7.0 (on refresh) on my Mac, strictly on the homepage. Maybe it clears up when we go live?

--- wp-content/themes/warmbuilding/lib/js/homepage-jquery.js
+++ wp-content/themes/warmbuilding/lib/js/homepage-jquery.js
@@ -1,7 +1,7 @@
 jQuery(document).ready(function($) {
 	var numwidgets = $('.footer-widgets-1 div.widget').length;
 	$('.footer-widgets-1').addClass('cols-'+numwidgets);
-	$('.footer-widgets-1 .widget,.footer-widgets-1 .widget .widget-wrap').equalHeights();
+	//$('.footer-widgets-1 .widget,.footer-widgets-1 .widget .widget-wrap').equalHeights();
 	$('.equalize').equalHeights();
 	
 	$('#hp-bot a.widget_sp_image-link').mouseenter(function(){
